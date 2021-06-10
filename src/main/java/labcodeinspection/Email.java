1fdb8d472213a5406f279cc27883fee4184b5151
Jonathan Vivas <jvivas.trecool@gmail.com>
2021-06-10 16:54:46 -0500

    Trabajo hecho
    No había guardado el reporte con los errores antes de corregir las violaciones, por eso solamente existe el reporte sin errores

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -2,7 +2,7 @@ package labcodeinspection;
 
 public class Email {
 
-	private String m_firstName;
+	private String m_firstName; //NOPMD Será manipulada luego
 	private String m_lastName;
 	private String password = null;
 	private String department;
